7d75ac8525b6e870df319d4c552233ac86e0165c
Vlad <vladyka219@gmail.com>
2020-12-12 14:23:17 +0300

    Add project files.

---
+++ Labs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+
+            for (int x = 1; x <= 50000; x++)
+            {
+                int y = (int) (x * Math.Exp(-x / 50000.0));
+
+                max = Math.Max(max, y);
+            }
+
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
